6bb1af46f15d95660f123371bb457495a6973cf3
Veronica Suprun <veronicasuprun@transfix.io>
2022-12-02 17:33:48 -0500

    fix day 1 sample to make sure the last line is counted

--- day1.rb
+++ day1.rb
@@ -1,14 +1,11 @@
-File.open('./day1.txt') do |file|
+File.open('./day1_sample.txt') do |file|
   max_elf_cals = 0
   current_elf_cals = 0
 
-  file.each_line do |line|
-    if line == "\n"
-      max_elf_cals = current_elf_cals if current_elf_cals > max_elf_cals
-      current_elf_cals = 0
-    else
-      current_elf_cals += line.to_i
-    end
+  file.each_line.with_index do |line, index|
+    current_elf_cals = line == "\n" ? 0 : current_elf_cals + line.to_i
+
+    max_elf_cals = current_elf_cals if current_elf_cals > max_elf_cals
   end
 
   puts max_elf_cals
--- day1_pt2.rb
+++ day1_pt2.rb
@@ -3,23 +3,18 @@ File.open('./day1.txt') do |file|
   current_elf_cals = 0
 
   file.each_line do |line|
-    if line == "\n"
+    current_elf_cals += line.to_i
 
-      if top_3_elf_cals.size < 3
-        top_3_elf_cals << current_elf_cals
-      else
-        if top_3_elf_cals.any? { |elf_cal| elf_cal < current_elf_cals }
-          top_3_elf_cals.shift
-          top_3_elf_cals << current_elf_cals
-        end
-      end
-
-
+    if line == "\n" || file.eof?
+      top_3_elf_cals << current_elf_cals
       top_3_elf_cals.sort!
+      top_3_elf_cals.shift if top_3_elf_cals.size > 3
+
       current_elf_cals = 0
-    else
-      current_elf_cals += line.to_i
     end
+
+
+
   end
 
   puts top_3_elf_cals.sum
