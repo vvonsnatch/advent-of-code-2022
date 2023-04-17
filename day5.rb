93bcd5ac39b9f228228c76dee5be5acf99935b75
Veronica Suprun <veronicasuprun@transfix.io>
2023-04-17 18:40:42 -0400

    replace day5 with day5_new

--- day5.rb
+++ day5.rb
@@ -1,68 +1,54 @@
-BOX_CHAR_SIZE = 3
 
-def print_stacks(stacks, level)
-  puts "LEVEL: #{level}"
-  puts "stack 1: #{stacks[0]}"
-  puts "stack 2: #{stacks[1]}"
-  puts "stack 3: #{stacks[2]}"
-  puts "stack 4: #{stacks[3]}"
-  puts "stack 5: #{stacks[4]}"
-  puts "stack 6: #{stacks[5]}"
-  puts "stack 7: #{stacks[6]}"
-  puts "stack 8: #{stacks[7]}"
-  puts "stack 9: #{stacks[8]}"
+FILENAME='day5_sample.txt'
+
+lines = File.open(FILENAME).readlines.map(&:chomp)
+
+# find index of line that lists the stacks
+stackIndex = nil
+lines.each_with_index do |line, index|
+  if line =~ /(\s+\d)+/
+    stackIndex = index
+    break
+  end
 end
 
-File.open('day5.txt') do |file|
+numStacks = lines[stackIndex].delete(' ').size
+
+# list of stacks
+stacks = []
+numStacks.times do
+  stacks << []
+end
+
+# construct stacks
+i = stackIndex - 1
+while i >= 0 do
+  stackElementIndex = 1
   stackIndex = 0
-  boxesLineSize = nil
-
-  lines = file.readlines
-  lines.each do |line|
-    if line =~ /(\s+\d)+/
-      boxesLineSize = line.size + 1
-      break
-    end
+  numStacks.times do
+    stackElement = lines[i][stackElementIndex]
+    stacks[stackIndex] << stackElement unless stackElement == " " || stackElement.nil?
     stackIndex += 1
+    stackElementIndex += 4
   end
-
-  # construct the stacks
-  stacks = []
-  stackLineWithoutBlanks = lines[stackIndex].gsub(/[[ ]]/,'')[0..-2]
-  numStacks = stackLineWithoutBlanks.size ## CAVEAT - ONLY WORKS FOR 9 SINGLE DIGIT LABELLED STACKS
-  numStacks.times { stacks << [] }
-
-  puts "STACK INDEX: #{stackIndex}"
-  puts "NUM STACKS: #{numStacks}"
-
-
-  #   read up one line at a time to populate the stacks
-  (stackIndex - 1 ).downto(0).each do |i|
-    boxes = lines[i]
-
-    j = 0
-    while j < boxesLineSize
-      box_label = boxes[(j+1)...(j+2)]
-      puts "BOX LABEL: #{box_label}"
-      puts "#{j} / #{BOX_CHAR_SIZE} = #{j / BOX_CHAR_SIZE}"
-      puts "stacks[#{j / BOX_CHAR_SIZE}]: #{stacks[j / BOX_CHAR_SIZE]}"
-      puts
-      stacks[j / BOX_CHAR_SIZE] << box_label
-      j += BOX_CHAR_SIZE + 1
-    end
-
-    print_stacks(stacks, stackIndex - i)
-  end
-
-  stacks = stacks.map { |stack| (stack - [" "]).compact }
-
-
-
-  # parse each line after the stacks to move things
-  #   x times: pop 1 from a and add 1 to b
-  # iterate over all of the stacks and get the topmost item
-
-
-
-
+  i -= 1
 end
+
+# move stack items
+instructions = lines[(stackIndex + 2)...lines.size]
+puts instructions
+
+
+
+
+
+puts "#{stacks[0]}"
+puts "#{stacks[1]}"
+puts "#{stacks[2]}"
+puts "#{stacks[3]}"
+puts "#{stacks[4]}"
+puts "#{stacks[5]}"
+puts "#{stacks[6]}"
+puts "#{stacks[7]}"
+puts "#{stacks[8]}"
+
